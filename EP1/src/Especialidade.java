9fabe30e2e41e8efafbc45542e4ec409fb5c4e5a
Jorgehtg <jgargalhone@gmail.com>
2025-09-08 23:50:21 -0300

    Create Especialidade.java

---
+++ EP1/src/Especialidade.java
@@ -0,0 +1,19 @@
+public class Especialidade {
+    private String nome;
+	
+	public Especialidade() {
+		this.nome = "";
+	}
+	
+	public Especialidade(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
