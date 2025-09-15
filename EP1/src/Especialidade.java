e0b447386920d8a34d04674ea0b58cab26ebcce5
Jorgehtg <jgargalhone@gmail.com>
2025-09-15 11:26:14 -0300

    Update Especialidade.java

--- EP1/src/Especialidade.java
+++ EP1/src/Especialidade.java
@@ -1,12 +1,15 @@
 public class Especialidade {
     private String nome;
+	private double valorConsulta;
 	
 	public Especialidade() {
 		this.nome = "";
+		this.valorConsulta = 0.0;
 	}
 	
-	public Especialidade(String nome) {
+	public Especialidade(String nome, double valorConsulta) {
 		this.nome = nome;
+		this.valorConsulta = valorConsulta;
 	}
 	
 	public String getNome() {
@@ -15,5 +18,13 @@ public class Especialidade {
 	
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public double getValorConsulta(){
+		return this.valorConsulta;
+	}
+
+	public void setValorConsulta(double valorConsulta){
+		this.valorConsulta = valorConsulta;
 	}
 }
