175aec0303dbc9d50edefb2e1145384337db5756
Jorgehtg <jgargalhone@gmail.com>
2025-09-15 11:48:36 -0300

    Create Paciente.java

---
+++ EP1/src/Paciente.java
@@ -0,0 +1,39 @@
+public class Paciente extends Pessoa {
+    private String cpf;
+    private int idade;
+    private String[] historicoConsulta;
+    private String[] historicoInternacao;
+
+    public Paciente(){
+        super();
+        this.cpf = "000000000-00";
+        this.idade = 0;
+        this.historicoConsulta = new String[0];
+        this.historicoInternacao = new String[0];
+    }
+
+    public Paciente(String nome, String cpf, int idade, String[] consultas, String[] internacoes){
+        super(nome);
+        this.cpf = cpf;
+        this.idade = idade;
+        this.historicoConsulta = consultas;
+        this.historicoInternacao = internacoes;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+    
+}
